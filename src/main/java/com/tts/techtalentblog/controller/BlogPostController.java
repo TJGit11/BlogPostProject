f68efe4045d6a16c5f8a3d16356a051787a1a09f
TJGit11 <74613952+TJGit11@users.noreply.github.com>
2021-02-10 10:52:10 -0600

    Working blog post form

--- src/main/java/com/tts/techtalentblog/controller/BlogPostController.java
+++ src/main/java/com/tts/techtalentblog/controller/BlogPostController.java
@@ -25,6 +25,14 @@ public class BlogPostController {
 
     @PostMapping("/")
     public String addNewBlogPost(BlogPost blogPost, Model model){
+        blogPostRepository.save(new BlogPost(
+                blogPost.getTitle(),
+                blogPost.getAuthor(),
+                blogPost.getBlogEntry()));
+                model.addAttribute("title", blogPost.getTitle());
+                model.addAttribute("author", blogPost.getAuthor());
+                model.addAttribute("blogEntry", blogPost.getBlogEntry());
+        return "blogpost/result";
 
     }
 
